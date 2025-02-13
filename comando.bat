906995227c5c8b538f80f6efd636a6d5d8342342
natdonangelo <natalia.d.nogueira@aluno.senai.br>
2025-02-13 15:01:23 -0300

    Add files via upload

--- comando.bat
+++ comando.bat
@@ -1,4 +1,25 @@
-mkdir pasta
-rmdir pasta
-mkdir "terceira pasta"
-echo "olá!" > arquivo.txt
+mkdir filmes
+cd filmes
+mkdir terror
+mkdir romance
+cd romance
+echo como perder um homem em 10 dias > filmesromanticos.txt
+echo 10 coisas que eu odeio em voce >> filmesromanticos.txt
+echo vestida para casar >> filmesromanticos.txt
+cd ../terror
+echo it a coisa > filmesassustadores.txt
+echo a casa de cera >> filmesassustadores.txt
+echo panico >> filmesassustadores.txt
+cd ..
+mkdir comedia
+cd comedia
+echo mistério no mediterrâneo > adamsandler.txt
+echo gente grande >> adamsandler.txt
+echo o amor é cego >> jackblack.txt
+echo a era do gelo >> jackblack.txt
+cd ..
+mkdir oscar
+cd oscar
+echo ainda estou aqui > premio.txt
+cd ..
+rename oscar racso
